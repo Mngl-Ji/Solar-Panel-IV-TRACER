33236c78c4a64256b82d336d23e8f11bfb9be700
Mohit Mangal <76986834+Mngl0Ji@users.noreply.github.com>
2021-09-16 14:27:47 +0530

    Update code_part1.ino

--- code_part1.ino
+++ code_part1.ino
@@ -3,6 +3,20 @@
 // Note :  This project requires NTC   probe and 10k resistor for temperature measurement, solar cell or panel as irradiance meter, button funtion and Datalogger shield (optional) to record data for I-V curve in excel.
 // Note :  The data can be saved into SD Card via datalogger shield to plot I-V curve in excel file. By default it comes with datalogger code. If you don't have the datalogger shield, kindly delete relevant codes or it might not function properly.
 
+       
+        
+        #include<SD.h>                              /* Load the SD card Library (by default already built-it with arduino solftware)*/
+        #include<SPI.h>                             /* Load the SPI of SPI communication Library (by default already built-it with arduino solftware)*/
+        
+        #include <WiFi.h>
+        #include <WiFiClient.h>
+        #include <WebServer.h>  //https://github.com/bbx10/WebServer_tng
+        #define I2C_SDA 32
+        #define I2C_SCL 33
+        const char* wifi_name = "Tenda_31BC98"; // Your Wifi network name here
+        const char* wifi_pass = "barcelona";    // Your Wifi network password here
+        WiFiServer server(80);    // Server will be at port 80
+
         /* 0- General */
 
         int decimalPrecision = 1;                   // decimal places for values (Pmp, Vmp, Voc, Temp and Irrad) in LED Display & Serial Monitor
@@ -41,41 +55,32 @@
 
         /* 6 Recording Calculation */
 
-        float PmpSolarPanel = 50;                   // Key in wattage of your solar panel at STC.
-        float reducedPmp =0 ;                       /* variable to calculated expected maximum power for a solar panel */
+        float PmpSolarPanel = 50;                   // Key in wattage of your solar panel at STC
         int InitiateReading = 0;                    /* To switch between functions for start and stop recording */   
         int InitialReset = 0;                       /* to reset the electrical parameters for the next measurement set */
         float recordLastSample=0;                   /* to count time for each sample.*/
-        float PmpInitial = 0;                       /* variable for recording Pmp */
-        float IscInitial = 0;                       /* variable for recording Isc */
-        float VocInitial = 0;                       /* variable for recording Voc */
-        float Vmp = 0;                              /* variable for Vmp */
-        float Imp = 0;                              /* variable for Imp */
+        float Power = 0;                       /* variable for recording Pmp */
+        float Isc = 0;                       /* variable for recording Isc */
+        float Voc = 0;                       /* variable for recording Voc */
+        float Vmp = 0;                              /* value of voltage for which the power will be maximum */
+        float Imp = 0;                              /* value of current for which the power will be maximum */
+        float Pmax = 0;                             /* Maximum value of power */
 
         /* 8 - SD memory card shield */
 
-        #include<SD.h>                              /* Load the SD card Library (by default already built-it with arduino solftware)*/
-        #include<SPI.h>                             /* Load the SPI of SPI communication Library (by default already built-it with arduino solftware)*/
         int chipSelect = 10;                        // Select ChipSelect pin based on the SPI communication for SC card shield. This pin cannot be changed.
         File mySensorData;                          // Variable for working with our file object
-        #include <WiFi.h>
-        #include <WiFiClient.h>
-        #include <WebServer.h>  //https://github.com/bbx10/WebServer_tng
-        #define I2C_SDA 32
-        #define I2C_SCL 33
-        const char* wifi_name = "Tenda_31BC98"; // Your Wifi network name here
-        const char* wifi_pass = "barcelona";    // Your Wifi network password here
-        WiFiServer server(80);    // Server will be at port 80
-         
 
 
         /*Now we will set the pins for the relay and the IGBTs switch*/
         int IGBTPin = 4; // 1st IGBT as a switch
         int IGBTPin1 = 6; //2nd IGBT as a switch(edit)
         int relay_pin = 2; //to control electronic relay
+
+
          void setup()                                        /* The Codes only run 1 time only when it started.*/
          
-                {
+              {
                 Serial.begin (115200);
                 pinMode (relay_pin, OUTPUT);
                 pinMode (IGBTPin, OUTPUT);
@@ -101,7 +106,7 @@
                 Serial.println("Type the above IP address into browser search bar"); 
                 server.begin();                           // Starting the server
                
-                }
+              }
         
           
         void loop()      
@@ -109,62 +114,67 @@
         
           {             
                          // IGBTPin1 is control the one IGBT connected directly with solar panel in series to calculate the short circuit currrent of solar panel
+                         digitalWrite(relay_pin, LOW);
                          digitalWrite(IGBTPin1, HIGH);
                          current = analogRead(CurrentAnalogInputPin); 
                          float  I = (((current /4095) *5000) /mVperAmpValue);  // Esp32 has ADC value of 12 bits
                          float Isc=I;
+                         delay(100)   
                          digitalWrite(IGBTPin1, LOW);
+                         
+                
                          //Now we will charge the capacitor by make the second IGBT ON, through which our current and voltage sensor can take a finite no. of reading to plot a graph
-                         if I>0
-                         {
-                         for(int i=0;i<=50;i++)
-                         {
                          digitalWrite(IGBTPin, HIGH);
-                         digitalWrite(relay_pin, LOW);
+                         
                          voltage = analogRead(VoltageAnalogInputPin);
-                         float  V = ((voltage*moduleSupplyVoltageV)/1024.0) / (R2/(R1+R2));                    /* Calculate the expected monitoring votlage */
+                         float  V = ((voltage*moduleSupplyVoltageV)/4095) / (R2/(R1+R2));                    /* Calculate the expected monitoring votlage */
                          current = analogRead(CurrentAnalogInputPin); 
-                         float  I = (((current /1024) *5000) /mVperAmpValue);                                      /* calculate the final RMS current*/ 
+                         float  I = (((current /4095) *5000) /mVperAmpValue);                                      /* calculate the final RMS current*/ 
                          float Power =  I * V;
+                         if(P > Pmax)
+                         {
+                                 Pmax = P;      Vmp = V;        Imp = I;
+                         }
+                         Serial.println("Voltage at Maximum Power");
+                         Serial.println(Vmp);
+                         Serial.println("Current at Maximum Power");
+                         Serial.println(Imp);
+                         Serial.println("Voltage");
                          Serial.println(V);
+                         Serial.println("Current");
                          Serial.println(I);
                          Serial.println(PowerValue);
-                         }
-                         }
-                         elseif I=0
-                         {
+                         delay(100);                 //Total time capacitor will take to charge fully 
                          digitalWrite(IGBTPin, LOW);
-                         float  Voc = ((analogRead(VoltageAnalogInputPin)*moduleSupplyVoltageV)/1024.0) / (R2/(R1+R2)); //This will measure the open source voltage of solar panel while the capacitor behaves as open circuit in fully charged condition
+                
+                         //Voc is  open source voltage of solar panel while the capacitor behaves as open circuit in fully charged condition
+                         float  Voc = ((analogRead(VoltageAnalogInputPin)*moduleSupplyVoltageV)/4095) / (R2/(R1+R2)); 
                          digitalWrite(relay_pin, HIGH);
-                         Serial.setTimeout(100);
-                         }
+                         delay(100);               // Time into which capacitor will discharge
+                         digitalWrite(relay_pin, LOW);
                          
                
           }      
           
           
           
-          /* 6 Recording & Calculation */
+          /* 6 Recording & Calculation 
         
                 if(InitiateReading == 1)                                                                    // If Left button is pressed to start measuring
                   {
-                    if(InitialReset == 1)                                                                   /* set all electrical parameter to 0 to start measuring */
+                    if(InitialReset == 1)                                                                   // set all electrical parameter to 0 to start measuring 
                       {
-                        PmpInitial = 0;
-                        IscInitial = 0;
-                        VocInitial = 0;
-                        Vmp = 0;
-                        Imp = 0;
                         InitialReset = 0;
                         mySensorData=SD.open("IVCur.txt",FILE_WRITE);                                       // Open or create IVCur.txt on the SD card as a file to write to
                         if(mySensorData)                                                                    // only to do these if data file opened sucessfully
                           {
-                           mySensorData.println("START");                                                   /* write the word "START" in the file */
+                           mySensorData.println("START");                                                   // write the word "START" in the file 
                            mySensorData.close();
                           }
                       }
-                  
-                        /* 8 - SD memory card shield */
+                   }     
+                        
+            8 - SD memory card shield 
                   
                         mySensorData=SD.open("IVCur.txt",FILE_WRITE);                                       // Open or create IVCur.txt on the SD card as a file to write to
                         if(mySensorData)                                                                    // only to do these if data file opened sucessfully
@@ -175,10 +185,5 @@
                             mySensorData.close();                                                           // close the file
                             Serial.println("written to SD Card !"); 
                           }
-                          
-                        recordLastSample = millis();
-                      }
-               
-                  }
-           }
-        
+                      
+          */
